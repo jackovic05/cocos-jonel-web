6b8ba0f8df41b056d2994249aedd0a316fdb2b7f
Jose Acevedo <joseacevedo0501@gmail.com>
2025-04-10 02:27:08 +0200

    Upgrade js, css, and main for smooth transition of navbar

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def inicio():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5001)
+    app.run(debug=True, port=5001)
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,31 @@
-// Ocultar el navbar al hacer scroll hacia abajo
 let lastScrollTop = 0;
 const navbar = document.getElementById("mainNav");
+let isNavigatingToAnchor = false;
 
+// Detectar clics en enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", () => {
+    isNavigatingToAnchor = true;
+    setTimeout(() => {
+      isNavigatingToAnchor = false;
+    }, 1000); // tiempo de espera antes de volver a permitir ocultar
+  });
+});
+
+// Ocultar el navbar al hacer scroll hacia abajo
 window.addEventListener("scroll", function () {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop) {
-    navbar.style.transform = "translateY(-100%)";
+  if (isNavigatingToAnchor) return;
+
+  if (scrollTop > lastScrollTop && scrollTop > 60) {
+    // Scroll hacia abajo → ocultar navbar
+    navbar.classList.add("hidden");
   } else {
-    navbar.style.transform = "translateY(0)";
+    // Scroll hacia arriba → mostrar navbar
+    navbar.classList.remove("hidden");
   }
 
   lastScrollTop = scrollTop;
 });
+
